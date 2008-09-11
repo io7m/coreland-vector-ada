521fc3e765428b5470980bca08d05bb4c38723c3
m0 <m0@localhost>
2008-09-11 18:27:51 +0100

    second attempt at fix...

--- UNIT_TESTS/add_double.adb
+++ UNIT_TESTS/add_double.adb
@@ -13,28 +13,27 @@ procedure add_double is
   base_a : constant v16.vector_d_t := (others => 1.0);
   base_b : constant v16.vector_d_t := (others => 2.0);
   base_r : v16.vector_d_t := (others => 3.0);
-  tmp_a  : v16.vector_d_t;
 
   testno : integer := 0;
 
   procedure sys_exit (e: integer);
   pragma import (c, sys_exit, "exit");
 
-  procedure fail (want, got: v16.scalar_d_t; index: integer) is
-    s_tnum: constant string := integer'image (testno);
-    s_index: constant string := integer'image (index);
-    s_want: constant string := v16.scalar_d_t'image (want);
-    s_got: constant string := v16.scalar_d_t'image (got);
+  procedure fail (want, got : v16.scalar_d_t; index: integer) is
+    s_tnum  : constant string := integer'image (testno);
+    s_index : constant string := integer'image (index);
+    s_want  : constant string := v16.scalar_d_t'image (want);
+    s_got   : constant string := v16.scalar_d_t'image (got);
   begin
     io.put_line ("[" & s_tnum & "][" & s_index & "] fail " & s_want & " /= " & s_got);
     sys_exit (1);
   end fail;
 
-  procedure pass (want, got: v16.scalar_d_t; index: integer) is
-    s_tnum: constant string := integer'image (testno);
-    s_index: constant string := integer'image (index);
-    s_want: constant string := v16.scalar_d_t'image (want);
-    s_got: constant string := v16.scalar_d_t'image (got);
+  procedure pass (want, got : v16.scalar_d_t; index: integer) is
+    s_tnum  : constant string := integer'image (testno);
+    s_index : constant string := integer'image (index);
+    s_want  : constant string := v16.scalar_d_t'image (want);
+    s_got   : constant string := v16.scalar_d_t'image (got);
   begin
     io.put_line ("[" & s_tnum & "][" & s_index & "] " & s_want & " = " & s_got);
   end pass;
@@ -57,16 +56,22 @@ begin
   -- add, in place
   --
 
-  tmp_a := base_a;
-  v16_add.d (tmp_a, base_b);
-  check (tmp_a);
+  declare
+    tmp_a : v16.vector_d_t := base_a;
+  begin
+    v16_add.d (tmp_a, base_b);
+    check (tmp_a);
+  end;
 
   --
   -- add, external storage
   --
 
-  tmp_a := base_a;
-  v16_add.d_ext (base_a, base_b, tmp_a);
-  check (tmp_a);
+  declare
+    tmp_a : v16.vector_d_t := base_a;
+  begin
+    v16_add.d_ext (base_a, base_b, tmp_a);
+    check (tmp_a);
+  end;
 
 end add_double;
